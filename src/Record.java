ef5ff410d9bba956919644e231bf8504eda78bb6
baileyg2016 <bspell20@vt.edu>
2018-09-21 10:43:21 -0400

    committing

---
+++ src/Record.java
@@ -0,0 +1,72 @@
+/**
+ * This is the abstract class for the record objects
+ * that will be reviewers or movies
+ * 
+ * @author Bailey Spell and Adam Tapp
+ * @version Milestone 1
+ * 
+ *
+ * @param <T>
+ *            The generic type for the class
+ */
+public class Record<T> {
+    /**
+     * The key for the hashTable that corresponds to the record
+     */
+    private T key;
+    /**
+     * Whether or not the record has been deleted from the table
+     */
+    private boolean tombstone;
+
+
+    /**
+     * The constructor for the record objects
+     * 
+     * @param key
+     *            The key to insert this into the hashTable
+     */
+    public Record(T key) {
+        this.key = key;
+        this.tombstone = false;
+    }
+
+
+    /**
+     * Sets the key for the record
+     * 
+     * @param key
+     *            The key for the record to be stored into the hashTable
+     */
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         The key of the record
+     */
+    public T getKey() {
+        return key;
+    }
+
+
+    /**
+     * If the record is deleted set the record as a hashTable
+     */
+    public void isTombstone() {
+        this.tombstone = true;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         Whether or not the record is a tombStone
+     */
+    public boolean getTombstone() {
+        return tombstone;
+    }
+}
